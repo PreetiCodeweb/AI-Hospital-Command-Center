6291a40b9af08461328508ff713c80162a36eb98
Saptarshi Jana <sapjana20007@gmail.com>
2026-08-25 22:22:55 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import App from './App';
+import { RecommendationInteractions } from '../components/RecommendationInteractions';
+
+export default function Page() {
+	return <><App /><RecommendationInteractions /></>;
+}
